88e82289208cef4eb95c7ac0199c20b3e0822c56
Emilia Carvajal <emilia.carvajalm@gmail.com>
2022-12-24 22:05:31 -0400

    Add code to print array on dev

---
+++ PrintArray/printArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array1 = new int[5] { 13, 53, 55, 7, 19 };
+
+        ImprimirArray(array1, 4);
+
+    }
+
+
+    public static void ImprimirArray(int[] array, int tamanio)
+    {
+        for (int i = 0; i < tamanio; i++)
+        {
+            Console.WriteLine("Array posicion: " + i + " Valor: " + array[i]);
+        }
+    }
+    
+}
